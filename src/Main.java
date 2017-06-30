dfc24572a9c0558be72039b71eea966df2f6b67a
small-manong <1350959589@qq.com>
2017-06-30 16:58:10 +0800

    Signed-off-by: 安卓恶意代码检测用例08

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+	public static void main(String[] args) {
+		// X轴
+		double[] x = {0,20,30};
+		double[] y = {0,20,30};
+		// Y轴
+		PolyPlot polyPlot = new PolyPlot(x,y);
+	}	
+}
